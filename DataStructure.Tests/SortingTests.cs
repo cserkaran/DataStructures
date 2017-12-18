e33c57232a23cde4e3938ba2d86f07d80413a579
Karan Sharma <karan@id.com.au>
2017-12-18 15:55:19 +1100

    InsertionSort alogrithm with Unit test infrastructure.

---
+++ DataStructure.Tests/SortingTests.cs
@@ -0,0 +1,55 @@
+﻿using DataStructures.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DataStructure.Tests
+{
+    public class SortingTests : IDisposable
+    {
+        List<int> list;
+        List<int> listAfterSorting;
+
+
+        public SortingTests()
+        {
+            list = new List<int> { 5,2,4,6,1,3 };
+            listAfterSorting = list.ToList();
+
+        }
+
+        public void Dispose()
+        {
+            list.Clear();
+            list = null;
+            GC.SuppressFinalize(this);
+        }
+
+        [Fact]
+        public void Test_InsertionSortAscending()
+        {
+            InsertionSort.SortAscending(listAfterSorting);
+            CheckIfListSortedAscending();
+        }
+
+        private void CheckIfListSortedAscending()
+        {
+            var isOrderedAscending = listAfterSorting.SequenceEqual(list.OrderBy(x => x));
+            Assert.True(isOrderedAscending);
+        }
+
+        [Fact]
+        public void Test_InsertionSortDescending()
+        {
+            InsertionSort.SortDescending(listAfterSorting);
+            CheckIfListSortedDescending();
+        }
+
+        private void CheckIfListSortedDescending()
+        {
+            var isOrderedDescending = listAfterSorting.SequenceEqual(list.OrderByDescending(x => x));
+            Assert.True(isOrderedDescending);
+        }
+    }
+}
---
+++ DataStructures/Sorting/InsertionSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Sorting
+{
+    public static class InsertionSort
+    {
+        public static void SortAscending(List<int> items)
+        {
+            for(int i = 1; i < items.Count; i++)
+            {
+                var key = items[i];
+                var j = i - 1;
+                while (j >= 0 && items[j] > key)
+                {
+                    items[j + 1] = items[j];  
+                    j--;
+                }
+                items[j + 1] = key;
+            }
+
+        }
+
+        public static void SortDescending(List<int> items)
+        {
+            for(int i = 1; i < items.Count; i++)
+            {
+                int j = i - 1;
+                var key = items[i];
+                while (j >= 0 && items[j] < key)
+                {
+                    items[j + 1] = items[j];
+                    j--;
+                }
+                items[j + 1] = key;
+
+            }
+        }
+
+    }
+}
